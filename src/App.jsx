5b456a3152d5b8a241fbadaf4e9756f7f3dbf21a
Daniel Callata <dannycallata@gmail.com>
2025-08-30 10:08:00 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+import { Routes, Route } from "react-router-dom";  // ⬅️ add this
 
 function App() {
   return (
