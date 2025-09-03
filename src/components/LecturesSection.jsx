fc48c37dfad072af75fdf348b01a84eac2abe984
Daniel Callata <dannycallata@gmail.com>
2025-09-02 21:20:16 -0400

    Update LecturesSection.jsx

--- src/components/LecturesSection.jsx
+++ src/components/LecturesSection.jsx
@@ -23,9 +23,9 @@ const lectures = [
 ];
 
 
-export const ProjectsSection = () => {
+export const LecturesSection = () => {
   return (
-    <section id="projects" className="py-24 px-4 relative">
+    <section id="lectures" className="py-24 px-4 relative">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
           Featured <span className="text-primary">Projects</span>
