2ddf9c4692389930af11d40d839f79ccde021006
Daniel Callata <dannycallata@gmail.com>
2025-09-26 01:58:22 -0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from 'vite-plugin-sitemap'
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
@@ -8,6 +9,9 @@ export default defineConfig({
   base: "/", 
   plugins: [
     react(),
+    sitemap({
+      hostname: 'https://danielcallata.com'
+    }),
     tailwindcss({
       config: {
         darkMode: "class",
